be9f03f0824556dd44c47e4a4203985e176a465b
vfxblender <vfxblender@gmail.com>
2026-08-19 23:46:22 -0500

    Add addon package entry point

---
+++ dead_simple_motion/__init__.py
@@ -0,0 +1,25 @@
+bl_info = {
+    "name": "Dead Simple Motion",
+    "author": "VFXBlender",
+    "version": (0, 1, 0),
+    "blender": (4, 2, 0),
+    "location": "View3D > Sidebar > Dead Simple",
+    "description": "Quick motion setup: Rotate, Orbit, FX, Spawn/Looper, and Follow",
+    "category": "Animation",
+}
+
+from . import properties, rotate, orbit, fx, spawn, follow, handlers, ui
+
+_MODULES = (properties, rotate, orbit, fx, spawn, follow, ui)
+
+
+def register():
+    for module in _MODULES:
+        module.register()
+    handlers.ensure_handlers()
+
+
+def unregister():
+    handlers.remove_handlers()
+    for module in reversed(_MODULES):
+        module.unregister()
